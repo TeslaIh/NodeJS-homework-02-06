1df588c959c24ca0869a9bf842d7a9dc158749fd
TeslaIh <87144117+TeslaIh@users.noreply.github.com>
2022-06-28 13:36:43 +0300

    without email

--- additionals/index.js
+++ additionals/index.js
@@ -1,7 +1,9 @@
 const newError = require("./newError");
 const ctrlWrapper = require("./ctrlWrapper");
+const sendMail = require("./sendMail");
 
 module.exports = {
   newError,
   ctrlWrapper,
+  sendMail,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,8 +4,9 @@ const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
 
-const { newError } = require("../../additionals");
+const { newError, sendMail } = require("../../additionals");
 
 const { User, schemas } = require("../../models/user");
 
@@ -29,12 +30,79 @@ router.post("/signup", async (req, res, next) => {
       throw newError(409, "Email in use");
     }
     const hashPassword = await bcrypt.hash(password, 10);
-    const avatarURL = gravatar.url(email); 
-    await User.create({ email, password: hashPassword, avatarURL });
+    const avatarURL = gravatar.url(email);
+            const verificationToken = nanoid();
+            await User.create({
+              email,
+              password: hashPassword,
+              avatarURL,
+              verificationToken,
+            });
+            const mail = {
+              to: email,
+              subject: "Confirm registration",
+              html: `<a target="_blank" 
+            href="localhost:3000/api/users/verify/${verificationToken}">
+                Press for confirm email
+            </a>`,
+            };
+    await sendMail(mail);
+
     res.status(201).json({
       user: {
         email,
       },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/verify/:verificationToken", async (req, res, next) => {
+  try {
+    const { verificationToken } = req.params;
+    const user = await User.findOne({ verificationToken });
+    if (!user) {
+      throw newError(404);
+    }
+    await User.findByIdAndUpdate(user._id, {
+      verificationToken: null,
+      verify: true,
+    });
+    res.json({
+      message: "Varification successful",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/verify", async (req, res, next) => {
+  try {
+    const { error } = schemas.verifyEmail.validate(req.body);
+    if (error) {
+      throw newError(400, "Email or password invalid");
+    }
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw newError(401);
+    }
+    if (user.verify) {
+      throw newError(400, "Verification has already been passed");
+    }
+    const mail = {
+      to: email,
+      subject: "Confirm regestration",
+      html: `<a 
+            target="_blank" 
+            href="localhost:3000/api/users/verify/${user.verificationToken}">
+                Press for confirm email
+            </a>`,
+    };
+    await sendMail(mail);
+    res.json({
+      message: "Verification email sent",
     });
   } catch (error) {
     next(error);
@@ -49,9 +117,15 @@ router.post("/login", async (req, res, next) => {
     }
     const { email, password } = req.body;
     const user = await User.findOne({ email });
+    
     if (!user) {
       throw newError(401, "Email is wrong");
     }
+
+    if (!user.verify) {
+      throw newError(401, "Email not verify");
+    }
+    
     const passwordCompare = await bcrypt.compare(password, user.password);
     if (!passwordCompare) {
       throw newError(401, "Password is wrong");
@@ -61,7 +135,7 @@ router.post("/login", async (req, res, next) => {
       id: user._id,
     };
 
-    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "2h" });
 
     await User.findByIdAndUpdate(user._id, { token });
 
